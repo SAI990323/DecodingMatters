cc10038b77216eba247de68fa6779c975cdebb6c
ABAABA <sai990323@gmail.com>
2024-09-04 20:55:18 +0800

    Update LogitProcesser.py

--- code/LogitProcesser.py
+++ code/LogitProcesser.py
@@ -1,4 +1,4 @@
-from transfomers.generation import LogitsProcessor
+from transformers.generation import LogitsProcessor
 from typing import Callable, Dict, Iterable, List, Optional, Tuple, Union
 import math
 import numpy as np
@@ -79,43 +79,6 @@ class CFEnhancedLogitsProcessor(LogitsProcessor):
         self.count=0
 
     
-    
-    def get_unconditional_logits(self, input_ids):
-        if self.unconditional_context["first_pass"]:
-            if self.unconditional_context["input_ids"] is None:
-                self.unconditional_context["input_ids"] = input_ids[:, -1:]
-            if self.unconditional_context["attention_mask"] is None:
-                self.unconditional_context["attention_mask"] = torch.ones_like(
-                    self.unconditional_context["input_ids"], dtype=torch.long
-                )
-            input_ids = self.unconditional_context["input_ids"]
-            attention_mask = self.unconditional_context["attention_mask"]
-            self.unconditional_context["first_pass"] = False
-        else:
-            attention_mask = torch.cat(
-                [
-                    self.unconditional_context["attention_mask"],
-                    torch.ones_like(input_ids[:, -1:], dtype=torch.long),
-                ],
-                dim=1,
-            )
-            if not self.unconditional_context["use_cache"]:
-                input_ids = torch.cat([self.unconditional_context["input_ids"], input_ids[:, -1:]], dim=1)
-            else:
-                input_ids = input_ids[:, -1:]
-            self.unconditional_context["input_ids"] = input_ids
-            self.unconditional_context["attention_mask"] = attention_mask
-
-        out = self.model(
-            input_ids,
-            attention_mask=attention_mask,
-            use_cache=self.unconditional_context["use_cache"],
-            past_key_values=self.unconditional_context["past_key_values"],
-        )
-        self.unconditional_context["past_key_values"] = out.get("past_key_values", None)
-
-        return out.logits
-    
     @add_start_docstrings(LOGITS_PROCESSOR_INPUTS_DOCSTRING)
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
         scores = torch.nn.functional.log_softmax(scores, dim=-1)
@@ -164,4 +127,3 @@ class CFEnhancedLogitsProcessor(LogitsProcessor):
         out = self.guidance_scale * (scores - cf_score) + cf_score
     
         return out
-    
