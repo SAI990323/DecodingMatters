2a780b412af4165a4e57ede105ecf971940f580f
ABAABA <sai990323@gmail.com>
2024-09-04 20:54:47 +0800

    Update evaluate.py

--- code/evaluate.py
+++ code/evaluate.py
@@ -2,25 +2,24 @@
 import pandas as pd
 import fire
 import torch
-torch.set_num_threads(1)
 import json
 import os
-os.environ['OPENBLAS_NUM_THREADS'] = '1'
-os.environ['OMP_NUM_THREADS'] = '1'
 from transformers import GenerationConfig,  AutoTokenizer
 from transformers import AutoModelForCausalLM
-from code.dataset import D3Dataset
+from dataset import  D3Dataset
 from transformers import  LogitsProcessorList, TemperatureLogitsWarper
-from code.LogitProcesser import CFEnhancedLogitsProcessor
+from LogitProcesser import CFEnhancedLogitsProcessor
 if torch.cuda.is_available():
     device = "cuda"
 else:
     device = "cpu"
-    
 P = 998244353
 MOD = int(1e9 + 9)
 import numpy as np
 
+def get_hash(x):
+    x = [str(_) for _ in x]
+    return '-'.join(x)
 
 
     
@@ -36,26 +35,27 @@ def main(
     K: int = 0,
     seed: int = 0,
     temperature: float=1.0,
-    length_penalty: float=1.0,
     guidance_scale: float=1.0,
+    length_penalty: float=1.0
 ):
-    model = AutoModelForCausalLM.from_pretrained(base_model, torch_dtype=torch.bfloat16)
+    category_dict = {"Office_Products": "office products", "Books": "books", "steam": "games", "CDs_and_Vinyl": "musics", "Toys_and_Games": "toys and games", "Video_Games": "video games", "Musical_Instruments": "music instruments", "Sports_and_Outdoors": "sports and outdoors", "Pet_Supplies": "pet supplies", "Arts_Crafts_and_Sewing": "arts products", "STEAM": "games" }
+    category = category_dict[category]
+    model = AutoModelForCausalLM.from_pretrained(base_model, torch_dtype=torch.bfloat16, use_flash_attention_2=True if base_model.lower().find("llama") > -1 else False)
     with open(info_file, 'r') as f:
         info = f.readlines()
         info = ["\"" + _.split('\t')[0].strip(' ') + "\"\n" for _ in info]
-        info = [f'''### Response:
+        item_name = info
+        info = [f'''### Response: 
 {_}''' for _ in info]
     
     tokenizer = AutoTokenizer.from_pretrained(base_model)
-    prefixID = [tokenizer(_).input_ids for _ in info]
-
-    def get_hash(x):
-        x = [str(_) for _ in x]
-        return '-'.join(x)
-
+    if base_model.lower().find("llama") > -1:
+        prefixID = [tokenizer(_).input_ids[1:] for _ in info]
+    else:
+        prefixID = [tokenizer(_).input_ids for _ in info]
+    
     hash_dict = dict()
     sasrec_dict = dict()
-
     for index, ID in enumerate(prefixID):
         ID.append(tokenizer.eos_token_id)
         for i in range(4, len(ID)):
@@ -85,12 +85,11 @@ def main(
         return []
 
     tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.pad_token_id = toenizer.eos_token_id
+    tokenizer.pad_token_id = tokenizer.eos_token_id
     tokenizer.padding_side = "left"
-    val_dataset=D3Dataset(train_file=test_data_path,candidate_file=train_file, tokenizer=tokenizer,max_len=2560, category=category, test=True,K=K, seed=seed)
+    val_dataset=D3Dataset(train_file=test_data_path, tokenizer=tokenizer,max_len=2560, category=category, test=True,K=K, seed=seed)
 
-    train_data = pd.read_csv(train_file)
-
+    
     if logits_file is not None:
         if not logits_file.endswith(".npy"):
             logits_file = None
@@ -99,7 +98,7 @@ def main(
         logits = np.load(logits_file)
         sasrec_logits = torch.tensor(logits).softmax(dim = -1)
         sasrec_logits = sasrec_logits[val_dataset.data['Unnamed: 0'].tolist()]
-    
+        
     encodings = [val_dataset.__getitem__(i) for i in range(len(val_dataset))]
     test_data = val_dataset.get_all()
 
@@ -118,18 +117,20 @@ def main(
             length_penalty=1.0,
             **kwargs,
     ):
-        maxLen = [len(_["input_ids"]) for _ in encodings]
-        
+        maxLen = max([len(_["input_ids"]) for _ in encodings])
+        maxLen2 = max([len(_["negative_prompt_ids"]) for _ in encodings])
+
         padding_encodings = {"input_ids": []}
 
-        for _ in encodings:
+        for  _ in encodings:
             L = len(_["input_ids"])
             padding_encodings["input_ids"].append([tokenizer.pad_token_id] * (maxLen - L) + _["input_ids"])
+            
         generation_config = GenerationConfig(
             num_beams=num_beams,
             length_penalty=length_penalty,
-            top_p=0.95,
-            top_k=10,
+            # top_p=0,
+            # top_k=10,
             num_return_sequences=num_beams,
             pad_token_id = model.config.pad_token_id,
             eos_token_id = model.config.eos_token_id,
@@ -147,20 +148,21 @@ def main(
                 tokenizer=tokenizer,
                 num_beams=num_beams
             )
-        
+            logits_processor = LogitsProcessorList([TemperatureLogitsWarper(temperature=temperature), ccc])
+
             generation_output = model.generate(
                 torch.tensor(padding_encodings["input_ids"]).to(device),
                 generation_config=generation_config,
                 return_dict_in_generate=True,
                 output_scores=True,
-                logits_processor=LogitsProcessorList([TemperatureLogitsWarper(temperature=temperature), ccc]),
+                logits_processor=logits_processor,
             )
     
         s = generation_output.sequences[:, L:]
         sequence_scores = [[0 for i in range(len(generation_output.scores))] for _ in range(num_beams)]
         for i in range(num_beams):
             for j in range(L, len(generation_output.sequences[i])):
-                beam_index = generation_output.beam_indices[i][j - 1]
+                beam_index = generation_output.beam_indices[i][j - L]
                 if beam_index != -1:
                     sequence_scores[i][j - L] = generation_output.scores[j - L][beam_index][generation_output.sequences[i][j]].item()
         scores = generation_output.sequences_scores.tolist()
@@ -181,11 +183,15 @@ def main(
     Flg=True
     scores = []
     seq_scores = []
+    import random
     for idx, encodings in enumerate(tqdm(new_encodings)):
         if logits_file is not None:
-            output, score, seq_score = evaluate(encodings, sasrec_logits[idx], temperature=temperature, guidance_scale=guidance_scale, length_penalty=length_penalty)
+            output, score, seq_score = evaluate(encodings, sasrec_logits[idx].to(device), temperature=temperature, guidance_scale=guidance_scale, length_penalty=length_penalty)
         else:
             output, score, seq_score = evaluate(encodings, cf_logits=None, temperature=temperature, guidance_scale=guidance_scale, length_penalty=length_penalty)
+        if idx == 0:
+            print(output)
+            print(score)
         outputs = outputs + output
         scores = scores+ score
         seq_scores.append(seq_score)
@@ -204,5 +210,8 @@ def main(
 
 if __name__ == '__main__':
     fire.Fire(main)
+
+
+
 
 
